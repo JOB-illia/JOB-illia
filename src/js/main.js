6fd8844498c850dd0e412dd70ddcb8e6e2133fa2
JOB <job.klenchys.i@gmail.com>
2020-04-03 16:33:11 +0200

    Header done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
+    $('.header__row-burger').on('click', function () {
+        $(this).toggleClass('header__row-burger--open');
+        $('.header__nav').slideToggle();
+    });
 
 });
