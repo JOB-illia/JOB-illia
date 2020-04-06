ded55eeee270a744fede7900aafe8c5705e03993
JOB <job.klenchys.i@gmail.com>
2020-04-06 21:28:39 +0200

    page product and animation

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,12 @@ $(document).ready(function () {
         $(this).toggleClass('header__row-burger--open');
         $('.header__nav').slideToggle();
     });
-
+    let videoURL = $('.video__container iframe').attr('src');
+    $('.play').on('click', function () {
+        let addedURL = '?rel=0&showinfo=0&autoplay=1';
+        let allURL = videoURL + addedURL;
+        $('.video__container iframe').attr('src', allURL);
+        $('.video__container').addClass('video__container--clickPlayStart');
+        $('.play, .video__bg').css('display', 'none');
+    })
 });
